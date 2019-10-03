36df23c26c07bc5d98ae3a1e97f393784a2e7186
Hai tran do <10a3nguyenanhhai@gmail.com>
2019-10-03 07:10:03 +0700

    execute proc and use prepare Statement

--- controllers/sinhvien.controller.js
+++ controllers/sinhvien.controller.js
@@ -3,12 +3,55 @@ const sql = require('mssql');
 const pool = require('../connectDB');
 
 module.exports.index = async function(req, res, next) {
-    await pool.connect();
     var requests = new sql.Request(pool);
-    var data = await requests.query("SELECT * FROM SINHVIEN");
+     await pool.connect();
+    // try {
+    //     //throw new Error('BROKEN');
+    //     await requests.query("INSERT INTO KHOA VALUES('basd', N'Aasdf', 20)");
+    // } catch(err) {
+    //     next(err);
+    // }
+    var result = await requests.query("SELECT * FROM SINHVIEN");
     res.render('sinhvien/index', {
         title: 'Sinh vien',
-        dsSinhVien: data.recordsets[0]
+        dsSinhVien: result.recordsets[0]
     });
+    
+    pool.close();
+};
+
+module.exports.perparedStatement = async function(req, res, next) {
+    await pool.connect();
+    var ps = new sql.PreparedStatement(pool);
+    ps.input('param', sql.Int);
+    try{
+        await ps.prepare('select @param as value');
+        var result = [];
+        for (let i = 0; i <=10; i++)
+        {
+            let data = await ps.execute({param: i});
+            result.push(data.recordset[0]);
+        }
+        res.send(result);
+        await ps.unprepare();
+    } catch(err) {
+        next(err);
+    }
+    pool.close();
+};
+
+module.exports.filter = async function(req, res, next) {
+    await pool.connect();
+    var requset = new sql.Request(pool);
+    requset.input('GT', sql.Bit, 0);
+    try {
+        var result = await requset.execute("SELECT_SINHVIEN_GT");
+        res.render('sinhvien/index', {
+            title: 'Sinh vien',
+            dsSinhVien: result.recordsets[0]
+        });
+    } catch(err) {
+        next(err);
+    }
     pool.close();
 }
--- routers/sinhvien.route.js
+++ routers/sinhvien.route.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/sinhvien.controller');
 const router = express.Router();
 
 router.get('/', controller.index);
+router.get('/ps', controller.perparedStatement);
+router.get('/filter', controller.filter);
 
 module.exports = router;
